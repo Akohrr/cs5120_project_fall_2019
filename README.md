3911eeb867f5fe4cd6012d9185d9f726a3a8dd1c
akohrr <atadoga.akoh@gmail.com>
2019-09-25 15:08:04 -0400

    implement insertion sort

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-CS 5120 Project 1A submission <br> <br>
-Name: Akoh Atadoga <br> <br> 
+# CS 5120 Project 1A submission
+
+
+## Name: **Akoh Atadoga**
+
+
 I used python to implement insertion sort
-The input data is in the `input_data` directory <br> <br>
-The implementation of insertion sort is in `main.py` file
+
+The implementation of insertion sort is in `main.py` file and sample input data is in the `input_data` directory <br> <br>
+
+To run insertion sort on the `input_data` directory, enter the following in the command line
+```
+python main.py --default
+```
+
+To run insertion sort on custom input data, enter the following into the command line:
+```
+python main.py -fp path/to/input_data
+```
+or 
+```
+python main.py --file-path path/to/input_data
+```
+
+
+For more information on how to generate the input data, you can view the custom implementation [here](https://repl.it/@AkohAtadoga/generatedataset)
--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import argparse
 import time
-
-
-parser = argparse.ArgumentParser(description='Run insertion sort algorithm on a set of input data')
-
-parser.add_argument('-fp', '--filepath', action='store', nargs='?', metavar='File path to input data',
-                    dest='file_path', required=True)
+import os
 
 
 def insertion_sort(input_data_file_path: str):
@@ -29,3 +24,33 @@ def insertion_sort(input_data_file_path: str):
 
     with open('output_data.txt', 'a') as writer:
         writer.write(f'{len(input_data)} {num_of_comparisons} {execution_time}\n')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Run insertion sort algorithm on a set of input data')
+
+    parser.add_argument('-fp', '--filepath',
+                        action='store',
+                        nargs='?',
+                        help='File path to input data',
+                        dest='file_path',
+                        metavar='path/to/file',
+                        )
+
+    parser.add_argument('--default',
+                        help='Run insertion sort on the input data provided',
+                        action='store_true',
+                        dest='default_mode',
+                        )
+    args = parser.parse_args()
+
+    if args.default:
+        input_dirs = os.listdir('input_data')
+        for folder in input_dirs:
+            n_n = os.listdir('input_data/{}'.format(folder))
+            for file in n_n:
+                print(f'Current file is: input_data/{folder}/{file}')
+                insertion_sort(f'input_data/{folder}/{file}')
+
+    if args.file_path is not None:
+        insertion_sort(args.file_path)
