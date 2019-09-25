e4580def36621a1bcd0337de145153769e020389
akohrr <atadoga.akoh@gmail.com>
2019-09-25 16:01:59 -0400

    added new flag for custom values to be passed through command line

--- README.md
+++ README.md
@@ -10,17 +10,25 @@ The implementation of insertion sort is in `main.py` file and sample input data 
 
 To run insertion sort on the `input_data` directory, enter the following in the command line
 ```
-python main.py --default
+python main.py
 ```
 
 To run insertion sort on custom input data, enter the following into the command line:
 ```
-python main.py -fp path/to/input_data
+python main.py -fp path/to/input_data --no-default
 ```
 or 
 ```
 python main.py --file-path path/to/input_data
 ```
 
-
-For more information on how to generate the input data, you can view the custom implementation [here](https://repl.it/@AkohAtadoga/generatedataset)
+To run insertion sort a list of values e.g 8 1 4, enter the following in the command line. The list of values have to separated by a space
+```
+python main.py --no-default -cv 8 1 4
+```
+or 
+To run insertion sort a list of values e.g 8 1 4, enter the following in the command line. The list of values have to separated by a space
+```
+python main.py --no-default --custom-values 8 1 4
+```
+For more information on how to generate the input data, you can view the custom implementation in `gen_input_data.py` or online: [here](https://repl.it/@AkohAtadoga/generatedataset)
--- main.py
+++ main.py
@@ -25,9 +25,15 @@ def insertion_sort(input_data_file_path: str):
     with open('output_data.txt', 'a') as writer:
         writer.write(f'{len(input_data)} {num_of_comparisons} {execution_time}\n')
 
+    return input_data
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run insertion sort algorithm on a set of input data')
+    help_message = """
+    Run insertion sort algorithm on a set of input data. To run insertion sort on only custom data, you have to use the
+    --no-default flag unless it would run on both the sample data and custom data
+    """
+    parser = argparse.ArgumentParser(description=help_message)
 
     parser.add_argument('-fp', '--filepath',
                         action='store',
@@ -37,14 +43,22 @@ if __name__ == '__main__':
                         metavar='path/to/file',
                         )
 
-    parser.add_argument('--default',
-                        help='Run insertion sort on the input data provided',
-                        action='store_true',
+    parser.add_argument('-cv', '--custom-values',
+                        action='store',
+                        nargs='*',
+                        type=int,
+                        help='List of numbers to sorted separated by space e.g 8 1 4',
+                        dest='custom_values',
+                        )
+
+    parser.add_argument('--no-default',
+                        help='Do not insertion sort on the input data provided',
+                        action='store_false',
                         dest='default_mode',
                         )
     args = parser.parse_args()
 
-    if args.default:
+    if args.default_mode:
         input_dirs = os.listdir('input_data')
         for folder in input_dirs:
             n_n = os.listdir('input_data/{}'.format(folder))
@@ -52,5 +66,15 @@ if __name__ == '__main__':
                 print(f'Current file is: input_data/{folder}/{file}')
                 insertion_sort(f'input_data/{folder}/{file}')
 
-    if args.file_path is not None:
+    if args.file_path:
         insertion_sort(args.file_path)
+
+    if args.custom_values:
+        file_name = 'custom_values_{0:.0f}.txt'.format(time.time())
+        with open(file_name, 'w+') as writer:
+            for val in args.custom_values:
+                writer.write('{}\n'.format(val))
+        print('Running insertion sort on {} would give {}'.format(args.custom_values, insertion_sort(file_name)))
+
+
+
