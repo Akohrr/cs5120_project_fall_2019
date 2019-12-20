418f78be62f2def9cb36b5d33e0d49eb88eb2446
akohrr <atadoga.akoh@gmail.com>
2019-12-20 18:23:53 -0500

    project 2c

--- README.md
+++ README.md
@@ -1,55 +1,43 @@
-# CS 5120 Project 2 submission
+# CS 5120 Project 2C submission
 
 
 ## Name: **Akoh Atadoga**
 
 Note: This program requires an installation of Python 3.7 or higher
 
-I used python to implement a solution to the Change-Making problem using:
-1) Greedy algorithm
-2) Straight forward Recursive algorithm
-3) Bottom-up dynamic algorithm
+I used python to implement a Breadth First Search (BFS) algorithm to
+find the minimum hop length between 2 nodes in a graph.
 
-On unix system run all the algorithms on the test cases given in the assignment and randomly generated test cases, as shown in `input_data.txt` file, enter the following in the command line
+On unix system, to run the BFS algorithms on randomly generated `directed graph`.<br>
+ Enter the following in the command line
 ```
 $ bash run.sh
 ```
 
-To run all the algorithms on custom input, enter the following into the command line:
-```
-$ python main.py [amount] [number_of_coins] [coins_as_space_seperated_values]
-```
-**Note**: 
-1) The order must be maintained for the algorithms to work.
-2) If all the data is not supplied, the default data in `input_data.txt` is used.
-
-
-To run all the algorithms on amount of 63 with a US coin system of 4 values which are 1 5 10 25, enter the following in the command line. The values have to separated by a space
-```
-$ python main.py 63 4 1 5 10 25
-```
 
 ### Note:
-After running all the algorithms on the input: the amount used, the coin system, number of coins used and execution time in milliseconds is recorded in `name_of_the_algorithm_solution_output.txt` file.
-For example, the output of the recursive algorithm is in `recursive_solution_output.txt`.  
-
-A line of output in `greedy_solution_output.txt` and `bottom_up_dynamic_solution_output.txt` would look like
-<br>`1 5 10 25`<br>
-`0.00044585`<br>
-. <br>
-. <br>
-.<br>
+After running all the BFS algorithms on the randomly generate graph, <br>
+The path from the start node to end node and the execution time in milliseconds is recorded in `output_data_{random_number}.csv` file. 
+ Where {random_number} is a random number from 0 to 100. For example `output_data_87.csv`<br> 
 <br>
-where the values on the top are coins used while
-the value below it is the time taken to execute the algorithm on a given set of data.<br>
+For example, running the program on a random graph below:<br>
+Edges of 1 is {'3', '2'}<br>
+Edges of 2 is {'3', '4'}<br>
+Edges of 3 is {'3', '4'}<br>
+Edges of 4 is {'1', '4'}<br>
+Graph is {'1': {'3', '2'}, '2': {'3', '4'}, '3': {'3', '4'}, '4': {'1', '4'}} <br>
+Number of nodes is 4 edges is 8 <br>
+Path from 1 to 4  is ['1', '3', '4'] <br>
 
-
-A line of output in `recursive_solution_output.txt` would look like
-<br>`4` <br> 
-`0.012159347534179688` <br>
-. <br>
-. <br>
-.<br>
+A line of output in `output_data_{random_number}.csv` using the example above would look like
+<br>`['1', '3', '4'], 0.0023598484`<br>
 <br>
- where the values on the top is the coin count
-the value below it is the time taken to execute the recursive algorithm on a given set of data<br>
+
+Note: The graphs are randomly generated so you would get different outputs each time you run the program.
+
+To graph used to generate the `output_data_{random_number}.csv` is stored in `graph_data_{random_number}.txt`. For example, `graph_data_87.txt`
+ would generate `output_data_87.csv` <br>
+The graph is stored to view the randomness of the graph generator and to verify the results.
+
+The graph for generating the project report is in `graph_data_99.txt`
+The output of the graph used for generating the project report is in `output_data_99.csv`
--- main.py
+++ main.py
@@ -1,46 +1,105 @@
-from minimum_coin_change_algorithms import greedy_solution, recursive_solution, bottom_up_dynamic_solution
-import sys
+import random
 import time
 
-algorithms = [recursive_solution, greedy_solution, bottom_up_dynamic_solution]
+
+# noinspection DuplicatedCode
+def generate_edges(no_of_edges, node, vertices, graph):
+    _edges = []
+    options = vertices[:]
+    for key, value in graph.items():
+        if node in value:
+            options.pop(options.index(key))
+
+    while len(_edges) < no_of_edges:
+        val = options[random.randint(0, len(options) - 1)]
+        _edges.append(val)
+        options.pop(options.index(val))
+    edges = set(_edges)
+    with open('generated_graph.txt', 'w+') as graph_writer:
+        graph_writer.write(f'Edges of {node} is {edges}')
+    return edges
 
 
-def run_all_algorithms(test_data, file_name):
-    for algorithm in algorithms:
-        for raw_data in test_data[:-1]:
-            data = raw_data.split(' ')
-            print(f'Running {algorithm.__name__} on {data}')
-            if len(data) > 1:
-                amount = int(data[0])
-                coin_system = [int(num) for num in data[2:]]
-                start_time = time.time()
-                coins_used = algorithm(amount, coin_system)
-                end_time = time.time()
-                execution_time = (end_time - start_time) * 1000  # time in milliseconds
-                if algorithm.__name__ == 'recursive_solution':
-                    # in the recursive algorithm, just report the coin count and time taken to run the algorithm
-                    with open(f'{algorithm.__name__}_{file_name}', 'a') as writer_1:
-                        writer_1.write(f"{len(coin_system)}\n{execution_time}\n")
-                else:
-                    # for other algorithms, report the coins used and time taken to run the algorithm
-                    with open(f'{algorithm.__name__}_{file_name}', 'a') as writer_1:
-                        writer_1.write(f"{' '.join([str(num) for num in coin_system])}\n{execution_time}\n")
-            else:
-                print('Wrong data format. Read the README.md file that accompanies the project')
+# noinspection DuplicatedCode
+def _generate_graph_helper(number_of_nodes, number_of_edges):
+    vertices = [str(_) for _ in range(1, number_of_nodes+1)]
+    graph = dict()
+    for vert in vertices:
+        graph[vert] = generate_edges(number_of_edges, vert, vertices, graph)
+    return graph
 
 
-def convert_input_data(file_to_use):
-    with open(file_to_use, 'r') as reader:
-        _data = reader.read().split('\n')
-        run_all_algorithms(_data, file_name='output.txt')
+# noinspection DuplicatedCode
+def get_number_of_edges(__graph):
+    no_of_nodes = 0
+    no_of_edges = 0
+    for key, values in __graph.items():
+        no_of_edges += len(values)
+        no_of_nodes += 1
+
+    return no_of_nodes, no_of_edges
+
+
+# noinspection DuplicatedCode
+def bfs_min_path(graph, start_node, end_node):
+    visited_path = []
+
+    queue = [[start_node]]
+
+    while len(queue) >= 1:
+        _path = queue.pop(0)
+        node = _path[-1]
+        if node not in visited_path:
+            children = graph[node]
+            for child in children:
+                path = list(_path)
+                path.append(child)
+                queue.append(path)
+                if child == end_node:
+                    return path
+
+            visited_path.append(node)
+
+    return None
+
+
+def generate_graph(number_of_nodes, number_of_edges):
+    generated_graph = None
+    _pass = 0
+    while _pass != 1:
+        try:
+            generated_graph = _generate_graph_helper(number_of_nodes, number_of_edges)
+            _pass = 1
+        except ValueError:
+            continue
+
+    size = get_number_of_edges(generated_graph)
+    print(f"Total number of node in generated graph is {size[0]}, edges is {size[1]} ")
+    return generated_graph
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 3:
-        post_fix = time.time()
-        file = 'input_data_{0:.0f}.txt'.format(post_fix)
-        with open(file, 'w') as writer:
-            writer.write(f'{" ".join(sys.argv[1:])}\n')
-        convert_input_data(file)
-    else:
-        convert_input_data('input_data.txt')
+    random_number_for_filename = random.randint(0, 100)
+    graphs_info_to_be_generated = {
+        '1': {'nodes': 30, 'edges': 60},
+        '2': {'nodes': 50, 'edges': 200},
+        '3': {'nodes': 100, 'edges': 1000},
+        '4': {'nodes': 200, 'edges': 4000},
+    }
+
+    for number, details in graphs_info_to_be_generated.items():
+        print(f'Processing {number}, {details}')
+        g = generate_graph(details["nodes"], details["edges"]//details["nodes"])
+        with open(f'graph_data_{random_number_for_filename}.txt', 'a') as writer:
+            writer.write(f"Graph {number} \n{{" + "\n".join("{!r}: {!r},".format(k, v) for k, v in g.items()) + "}\n")
+        _start = random.randint(1, details["nodes"])
+        _end = random.randint(_start, details["nodes"])
+        print(f"Start node is {_start}, end node is {_end}")
+        start_time = time.time()
+        shortest_path = bfs_min_path(g, str(_start), str(_end))
+        end_time = time.time()
+        time_taken = (end_time - start_time) * 1000  # time in milliseconds
+        with open(f'output_data_{random_number_for_filename}.csv', 'a') as writer:
+            writer.write(f"{shortest_path}, {time_taken}\n")
+
+    print('Program completed successfully :).')
